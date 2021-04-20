f68fd5295f98becef72f0526a27794079ec7828c
Jasp402 <jasp402@gmail.com>
2021-04-19 23:25:03 -0500

    v0.1.3 | fix - WAPI.getMe() function and refactory

--- src/lib/WAPI.js
+++ src/lib/WAPI.js
@@ -17,21 +17,21 @@ if (!window.Store) {
 		function getStore(modules) {
 			let foundCount = 0;
 			let neededObjects = [
-				{ id: "Store", conditions: (module) => (module.default && module.default.Chat && module.default.Msg) ? module.default : null },
-				{ id: "MediaCollection", conditions: (module) => (module.default && module.default.prototype && module.default.prototype.processAttachments) ? module.default : null },
-				{ id: "MediaProcess", conditions: (module) => (module.BLOB) ? module : null },
-				{ id: "Wap", conditions: (module) => (module.createGroup) ? module : null },
-				{ id: "ServiceWorker", conditions: (module) => (module.default && module.default.killServiceWorker) ? module : null },
-				{ id: "State", conditions: (module) => (module.STATE && module.STREAM) ? module : null },
-				{ id: "WapDelete", conditions: (module) => (module.sendConversationDelete && module.sendConversationDelete.length == 2) ? module : null },
-				{ id: "Conn", conditions: (module) => (module.default && module.default.ref && module.default.refTTL) ? module.default : null },
-				{ id: "WapQuery", conditions: (module) => (module.queryExist) ? module : ((module.default && module.default.queryExist) ? module.default : null) },
-				{ id: "CryptoLib", conditions: (module) => (module.decryptE2EMedia) ? module : null },
-				{ id: "OpenChat", conditions: (module) => (module.default && module.default.prototype && module.default.prototype.openChat) ? module.default : null },
-				{ id: "UserConstructor", conditions: (module) => (module.default && module.default.prototype && module.default.prototype.isServer && module.default.prototype.isUser) ? module.default : null },
-				{ id: "SendTextMsgToChat", conditions: (module) => (module.sendTextMsgToChat) ? module.sendTextMsgToChat : null },
-				{ id: "SendSeen", conditions: (module) => (module.sendSeen) ? module.sendSeen : null },
-				{ id: "sendDelete", conditions: (module) => (module.sendDelete) ? module.sendDelete : null }
+				{ id: "Store",            conditions: (module) => (module.default && module.default.Chat && module.default.Msg) ? module.default : null },
+				{ id: "MediaCollection",  conditions: (module) => (module.default && module.default.prototype && module.default.prototype.processAttachments) ? module.default : null },
+				{ id: "MediaProcess",     conditions: (module) => (module.BLOB) ? module : null },
+				{ id: "Wap",              conditions: (module) => (module.createGroup) ? module : null },
+				{ id: "ServiceWorker",    conditions: (module) => (module.default && module.default.killServiceWorker) ? module : null },
+				{ id: "State",            conditions: (module) => (module.STATE && module.STREAM) ? module : null },
+				{ id: "WapDelete",        conditions: (module) => (module.sendConversationDelete && module.sendConversationDelete.length == 2) ? module : null },
+				{ id: "Conn",             conditions: (module) => (module.default && module.default.ref && module.default.refTTL) ? module.default : null },
+				{ id: "WapQuery",         conditions: (module) => (module.queryExist) ? module : ((module.default && module.default.queryExist) ? module.default : null) },
+				{ id: "CryptoLib",        conditions: (module) => (module.decryptE2EMedia) ? module : null },
+				{ id: "OpenChat",         conditions: (module) => (module.default && module.default.prototype && module.default.prototype.openChat) ? module.default : null },
+				{ id: "UserConstructor",  conditions: (module) => (module.default && module.default.prototype && module.default.prototype.isServer && module.default.prototype.isUser) ? module.default : null },
+				{ id: "SendTextMsgToChat",conditions: (module) => (module.sendTextMsgToChat) ? module.sendTextMsgToChat : null },
+				{ id: "SendSeen",         conditions: (module) => (module.sendSeen) ? module.sendSeen : null },
+				{ id: "sendDelete",       conditions: (module) => (module.sendDelete) ? module.sendDelete : null }
 			];
 			for (let idx in modules) {
 				if ((typeof modules[idx] === "object") && (modules[idx] !== null)) {
@@ -599,10 +599,10 @@ window.WAPI.getGroupAdmins = async function (id, done) {
  * @returns {Array|*|$q.all}
  */
 window.WAPI.getMe = function (done) {
-	const rawMe = window.Store.Contact.get(window.Store.Conn.me);
+	const rawMe = window.Store.Contact.get(window.Store.Conn.wid._serialized);
 	
-	if (done !== undefined) done(rawMe.all);
-	return rawMe.all;
+	if (done !== undefined) done(rawMe);
+	return rawMe;
 };
 
 window.WAPI.isLoggedIn = function (done) {
